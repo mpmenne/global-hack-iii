728371fdd572c44697a48953c34cef708433e08d
MoMenne <mpmenne@electronicsystemssupport.com>
2014-12-06 14:11:36 -0600

    adding in the phone controller

--- src/main/java/com/globalhack/PhoneController.java
+++ src/main/java/com/globalhack/PhoneController.java
@@ -2,8 +2,12 @@ package com.globalhack;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,12 +22,21 @@ public class PhoneController {
 
     @Autowired
     @Lazy
-    private ExampleRepository exampleRepository;
+    private PhoneRepository phoneRepository;
 
-    @RequestMapping("/")
-    public String home() {
-        new Phone("3144779816");
-        return "Heyo";
+    @RequestMapping("/phones")
+    public List<Phone> phones() {
+        return phoneRepository.findAll();
+    }
+
+    @RequestMapping(value = "/phones", method = RequestMethod.POST)
+    public Phone phoneByNumber(String phoneNumber) {
+        return new Phone(phoneNumber);
+    }
+
+    @RequestMapping(value = "/phone/{phoneNumber}", method = RequestMethod.DELETE)
+    public void deletePhone(@PathVariable String phoneNumber) {
+        phoneRepository.delete(phoneRepository.findByPhoneNumber(phoneNumber));
     }
 
 }
